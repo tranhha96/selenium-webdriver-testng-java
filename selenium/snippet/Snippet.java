b6f696c102efa310920b727bd2843659915e1375
Admin <Admin@Admin>
2022-04-27 21:58:24 +0700

    Create new repository

---
+++ selenium/snippet/Snippet.java
@@ -0,0 +1,36 @@
+package snippet;
+
+public class Snippet {
+	What specific features are you interested in using?
+	Select all that apply so we can point you to the right GitHub plan.
+	
+	
+	GitHub Collaboration Icon
+	Collaborative coding
+	Codespaces, Pull requests, Notifications, Code review, Code review assignments, Code owners, Draft pull requests, Protected branches, and more.
+	
+	GitHub Automation and CI/CD Icon
+	Automation and CI/CD
+	Actions, Packages, APIs, GitHub Pages, GitHub Marketplace, Webhooks, Hosted runners, Self-hosted runners, Secrets management, and more.
+	
+	GitHub Security Icon
+	Security
+	Private repos, 2FA, Required reviews, Required status checks, Code scanning, Secret scanning, Dependency graph, Dependabot alerts, and more.
+	
+	GitHub Client Apps Icon
+	Client Apps
+	GitHub Mobile, GitHub CLI, and GitHub Desktop.
+	
+	GitHub Project Management Icon
+	Project Management
+	Projects, Labels, Milestones, Issues, Unified Contribution Graph, Org activity graph, Org dependency insights, Repo insights, Wikis, and GitHub Insights.
+	
+	GitHub Team Administration Icon
+	Team Administration
+	Organizations, Invitations, Team sync, Custom roles, Domain verification, Audit Log API, Repo creation restriction, and Notification restriction.
+	
+	GitHub Community Icon
+	Community
+	GitHub Marketplace, GitHub Sponsors, GitHub Learning Lab, Electron, and Atom.
+}
+
